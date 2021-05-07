fafcb52f39030d619ef24a351ce400c533e09260
Yasir Ali <aliyasirali@gmail.com>
2021-05-07 16:39:38 +0200

    Init commit
    Basic project structure added.

---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '13.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'SocialApp' do
+  
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Storage'
+  pod 'Kingfisher'
+  pod 'Firebase/Firestore'
+  pod 'ALLoadingView'
+
+end
---
+++ SocialApp/Presenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by SWE-PC-110 on 2021-05-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
